73c40cfc15e91e22c387b0044cf231e222c232ca
siddhant-glitch <55294144+siddhant-glitch@users.noreply.github.com>
2020-10-08 12:33:04 -0400

    visual changes made

--- js/modules/DataModule.js
+++ js/modules/DataModule.js
@@ -1,5 +1,29 @@
-export default = {
-    name: "Trevor",
-    role: "Prof",
-    nickname: "Tervor"
+export default {
+
+    Justin: {
+        name: "justin",
+        role: "coordinator",
+        nickname: "niceguy"
+    },
+
+    John: {
+        name: "John",
+        role: "super chill",
+        nickname: "just john"
+    },
+
+    Joe: {
+        name: "Joe",
+        role: "prof",
+        nickname: "teddy bear"
+    },
+
+    Pan: {
+        name: "Pan",
+        role: "Code ninja",
+        nickname: "spider pan"
+    }
+
+
+
 }
