2b2860b56904a321687d7f8ff487b06bfd6da327
siddhant-glitch <55294144+siddhant-glitch@users.noreply.github.com>
2020-10-15 13:54:20 -0400

    adding ajax

--- js/main.js
+++ js/main.js
@@ -1,17 +1,49 @@
 // import your packages here
-import Team from "./modules/DataModule.js";
+// import Team from "./modules/DataModule.js";
 
 (() => {
     // stub * just a place for non-component-specific stuff
+
+    // set up the XMLHttp object
+    let myReq = new XMLHttpRequest;
+    myReq.addEventListener("readystatechange", handleRequest);
+
+    //open a request and pass thru the url of the data that we want
+    myReq.open('GET', '../DataSet.json');
+
+    //actually make the request
+    myReq.send();
+    //handleRequest function goes here
+    function handleRequest() {
+debugger;
+
+        if (myReq.readyState === XMLHttpRequest.DONE) {
+            debugger;
+            //check status here and proceed
+            if (myReq.status == 200) {
+                //200 means done and dusted, ready to go with the dataset!
+                handleDataSet(myReq.responseText);
+            } else {
+                //probably got some kind of error code, so handle that 
+                //a 404, 500 ...can render appropriate messages here
+                console.error(`${myReq.status} : something done broke, son`);
+            }
+        } else {
+            debugger;
+            //request isn't ready yet, keep waiting...
+            console.log(`Request state: ${myReq.readyState}. Still processing...`);
+        }
+    }
+
     let userSection = document.querySelector(".users-section");
     let userTemplate = document.querySelector("#profs-template").content;
     
 
     //select our user element and load the content
     function handleDataSet(data) {
-        
 
-        for (let user in data) {
+        for (let user in myData) {
+
             let currentUser = userTemplate.cloneNode(true);
             let currentUserText = currentUser.querySelector('.user').children;
 
